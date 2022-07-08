a4fe1e1a7d1082c9ba67ed5cfe2991f758036665
uxconor <conor.ux@gmail.com>
2022-07-08 16:46:53 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GlobalStyles from './styles';
+import Pages from './pages';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  // uri of our GraphQL server
+  uri: 'http://localhost:4000',
+  // this setting stores query results using an in-memory cache
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
